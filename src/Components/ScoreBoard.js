9226e1d00dcbcc0b02ba1fcda6211688bd1a76d7
Michael Davison <mdavison92@Hotmail.com>
2021-05-25 11:53:22 -0400

    Added text

--- src/Components/ScoreBoard.js
+++ src/Components/ScoreBoard.js
@@ -1,16 +1,22 @@
 //render tasks
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 
 
 
 
 const ScoreBoard  = (props) => {
 
- 
+    const [currentScore, setCurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
 
    
     return (
-     <h1></h1>
+        <div id ="Scoreboard">
+     <h2>Current Score: {currentScore}</h2>
+     <h2>Best Score: {bestScore}</h2>
+     <h3>Goal: Click as many cards as </h3>
+     <h3>you can without clicking the same one twice!</h3>
+     </div>
     );
  
 
