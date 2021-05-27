865b2d66e9a215e33efdabccb7d190a8e47850c1
Michael Davison <mdavison92@Hotmail.com>
2021-05-27 00:43:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList';
-import ScoreBoard from './Components/ScoreBoard';
 
 
 
@@ -15,7 +14,6 @@ function App() {
   return (
     <div className="App">
         <h1>Memory Game</h1>
-      
         <h4 id = "tipHeader">Goal: Click as many cards as you can without clicking the same one twice! </h4>
         <CardList/>
        
