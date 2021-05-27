25cac49cdb780e3f1aac1eaa44eb7e7d9977cb27
Michael Davison <mdavison92@Hotmail.com>
2021-05-26 22:51:06 -0400

    Implemented resetGame(), clickHandler(), gameCheck() and more resource images

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -10,32 +10,31 @@ import maokai from '../Resources/maokai.jpg';
 import ryze from '../Resources/ryze.jpg';
 import lux from '../Resources/lux.jpg';
 
-//import AnneBoonchuy from '../images/anne.jpg';
-
-
-
-
-
 
 
 const CardList  = (props) => {
 
     const cards = [
-		{ image: caitlyn, name: 'Caitlyn' },
-		{ image: missFortune, name: 'Miss Fortune' },
-		{ image: swain, name: 'Swain' },
+		{ image: caitlyn, name: 'Caitlyn'},
+		{ image: missFortune, name: 'Miss Fortune'},
+		{ image: swain, name: 'Swain'},
         { image: anivia, name: 'Anivia'},
         { image: ekko, name: 'Ekko'},
         { image: jhin, name: 'Jhin'},
         { image: lulu, name: 'Lulu'},
         { image: maokai, name: 'Maokai'},
         { image: ryze, name: 'Ryze'},
-        { image: lux, name: 'Lux'}
+        { image: lux, name: 'Lux'},
 	];
+
+    const [cardsClicked, setCardsClicked] = useState([]);
 
     const [positions, setPositions] = useState(
 		cards.map((value, index) => index)
 	);
+
+    const [currentScore, setCurrentScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
 
 
 
@@ -51,8 +50,6 @@ const shuffleArray = (array) => {
     return shuffledArray;
 };
 
-
-
 /*
 
 const shuffleArray2 = (array) => {
@@ -64,32 +61,67 @@ const shuffleArray2 = (array) => {
    */
     const randomizeCards = () => {
 
-       console.log("test");
+       
     setPositions(shuffleArray(positions));
     
     }
-  
 
-   
+    const clickHandler = (cardName) => {
+        let cardNamePassed = cardName;
+        randomizeCards();
+        //call gameCheck
+        gameCheck(cardNamePassed);
+      
+    
+       
+        //checks if card.count is already clicked
+    }
 
+    const gameCheck = (cardName) => {
+      
+           if(cardsClicked.includes(cardName))
+           {
+              if(highScore < currentScore)
+                {
+                    setHighScore(currentScore);
+                }
+               resetGame();
+           }
+           else {
+               
+               setCardsClicked(cardsClicked => [...cardsClicked, cardName]);
+               console.log("Pushed " + cardName);
+               console.log(cardsClicked);
+               setCurrentScore(currentScore + 1);
+           }
+            
+    }
 
-
+    const resetGame = () => {
+        //reset positions to default
+        setPositions(cards.map((value, index) => index));
+        setCardsClicked([]);
+        setCurrentScore(0);
+        alert("OOPS! Game Over");
+    }
 
     return (
 
       <div id = "cardListBox">
-          
+          <div id="scoreBoard" >
+          <h4>Current Score: {currentScore}</h4>
+          <h4>High Score: {highScore}</h4>
+          </div>
           {positions.map((position) => {
 				return (
 					<div>
                         
-                		<img key = {cards[position].name} src = {cards[position].image} onClick={randomizeCards}></img>
+                		<img key = {cards[position].name} src = {cards[position].image} onClick={e=> clickHandler(cards[position].name)}></img>
                         <h2>{cards[position].name}</h2>
 					</div>
 				);
 			})}
-            
-            
+  
       </div>
        
     );
@@ -97,9 +129,4 @@ const shuffleArray2 = (array) => {
 
  };
 
- /*
-
- */
-
- 
 export default CardList;
