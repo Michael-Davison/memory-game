d622366fb8b4eef4d72d5c6eb8a54ff70f9b2fb6
Michael Davison <mdavison92@Hotmail.com>
2021-05-26 22:51:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import ScoreBoard from './Components/ScoreBoard';
 
 function App() {
 
- 
+  const resetGame = () => {
+    
+  }
+
   return (
     <div className="App">
         <h1>Memory Game</h1>
-        <ScoreBoard/>
+      
         <h4 id = "tipHeader">Goal: Click as many cards as you can without clicking the same one twice! </h4>
         <CardList/>
        
